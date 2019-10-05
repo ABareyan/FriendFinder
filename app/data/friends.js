36d6f601484c29aca8614fe93bbb5daacf413d30
Arman Bareyan <abareyan@gmail.com>
2019-10-04 22:59:17 -0600

    Update Files

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+var friends = [
+
+    {
+        name: "Jessica Alba",
+        photo: "https://media.self.com/photos/57d8990e4b76f0f832a0e074/4:3/w_728,c_limit/jessica-alba-blue-skirt.jpg",
+        scores: [5, 3, 2, 5, 5, 4, 3, 5]
+    },
+    {
+        name: "Uma Thuirman",
+        photo: "https://i.pinimg.com/originals/22/cb/00/22cb0098562bc1360ba47d55cca6169d.jpg",
+        scores: [2, 1, 4, 4, 5, 1, 2, 5]
+    },
+    {
+        name: "Irina Shayk",
+        photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/irina-shayk-walks-the-runway-wearing-ermanno-scervino-at-news-photo-1155752082-1560520826.jpg?crop=1.00xw:0.667xh;0,0.00795xh&resize=480:*",
+        scores: [5, 5, 4, 4, 5, 3, 1, 5]
+    },
+    {
+        name: "Greta Gaterman",
+        photo: "https://mediaslide-europe.storage.googleapis.com/eastwest/pictures/1709/5654/large-1505215779-08b9cf955b497d8d21134025aba7c90d.jpg",
+        scores: [5, 4, 3, 1, 4, 4, 2, 5]
+    },
+    {
+        name: "Naomi Campbell",
+        photo: "https://pbs.twimg.com/media/C4Jy39bW8AAkxEZ.jpg",
+        scores: [2, 1, 4, 3, 2, 3, 3, 2]
+    }
+];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,72 @@
+var friends = require("../data/friends");
+
+module.exports = function(app) {
+    app.get("/api/friend", function(req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friend", function(req, res) {
+        var totalDifference = 0;
+        var bestChoise = {
+            name: "",
+            photo: "",
+            difference: 500
+        };
+
+        var friendData = req.body;
+        var friendName = friendData.name;
+        var friendScore = friendData.scores;
+
+        var num = friendScore.map(function(item) {
+            return parseInt(item);
+        });
+        friendData = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: num
+        };
+        console.log("Name: " + friendName);
+        console.log("Score: " + friendScore);
+        console.log("=============================================");
+
+
+        var sum = num.reduce((a, b) => a + b);
+
+        console.log("Friend score: " + sum);
+        console.log("Best choise: " + bestChoise.difference);
+        console.log("=============================================");
+
+
+
+        for (var i = 0; i < friends.length; i++) {
+            console.log("Friend name: " + friends[i].name);
+
+            totalDifference = 0;
+            console.log("Tottal diff: " + totalDifference);
+            console.log("Best choise: " + bestChoise.difference);
+
+            var bestScore = friends[i].scores.reduce((a, b) => a + b);
+
+            console.log("Friend total score: " + bestScore);
+
+            totalDifference += Math.abs(sum - bestScore);
+
+            console.log(totalDifference + ' total difference');
+
+            if (totalDifference <= bestChoise.difference) {
+                bestChoise.name = friends[i].name;
+                bestChoise.photo = friends[i].photo;
+                bestChoise.difference = totalDifference;
+            }
+
+            console.log(totalDifference + ' total difference');
+            console.log("=======================================");
+
+        }
+        console.log(bestChoise);
+        friends.push(friendData);
+        console.log("New friend added");
+        console.log(friendData);
+        res.json(bestChoise);
+    });
+};
